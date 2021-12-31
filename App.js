9e4cc91174d26ceb6dbf80ce982d491712f4c62a
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-01-01 03:18:06 +0530

    [UI] Add Input Wrapper

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from "react-native"
-import Task from './components/Task'
+import { Platform } from "expo-modules-core";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
@@ -7,10 +8,21 @@ export default function App() {
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks!</Text>
         <View style={styles.items}>
-          <Task text='ITEM 1' />
-          <Task text='ITEM 2' />
+          <Task text="ITEM 1" />
+          <Task text="ITEM 2" />
         </View>
       </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder="Write a task" />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -30,5 +42,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#C0C0C0',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
